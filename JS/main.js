8ad016a903a2b447646ec4f6048189f70d806bfd
Ivan <ragna.blood82@gmail.com>
2022-01-10 17:47:14 +0300

    Js update

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,14 @@
-const paraf = document.getElementById('par');
+const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
